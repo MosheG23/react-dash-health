644bd53311af67e0a3c89bad9464e1f3b4dcce44
Moshe Gotam <56716267+MosheG23@users.noreply.github.com>
2020-10-30 16:16:51 +0200

    Updates

---
+++ precache-manifest.7994a7e59714446bd2af9e02e74dffd2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "586e82b85fa255857f708e7936db9681",
+    "url": "/react-dash-health/index.html"
+  },
+  {
+    "revision": "6fea7d739a07184eaf1b",
+    "url": "/react-dash-health/static/css/main.ea5ef615.chunk.css"
+  },
+  {
+    "revision": "1ab8fa356f686dd5a13e",
+    "url": "/react-dash-health/static/js/2.41a03d1c.chunk.js"
+  },
+  {
+    "revision": "e3cddcc14d0b349b4e6eccba623e55ae",
+    "url": "/react-dash-health/static/js/2.41a03d1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fea7d739a07184eaf1b",
+    "url": "/react-dash-health/static/js/main.ad837df3.chunk.js"
+  },
+  {
+    "revision": "4db4932c9d83ad077753",
+    "url": "/react-dash-health/static/js/runtime-main.cfb3361f.js"
+  },
+  {
+    "revision": "797c19eb0537c48c6c7a3a31953b7553",
+    "url": "/react-dash-health/static/media/countries.797c19eb.geojson"
+  },
+  {
+    "revision": "12c1307210448ceff93bfb82d4ac9685",
+    "url": "/react-dash-health/static/media/map_logo.12c13072.png"
+  }
+]);
